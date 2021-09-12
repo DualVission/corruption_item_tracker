dd95d199a3e58e99fad471b9d018be8c9d34aa2b
Zach the DualVission <zach@yarnot.com>
2021-09-11 19:47:16 -0500

    Made work

---
+++ scripts/init.lua
@@ -0,0 +1,8 @@
+Tracker:AddItems("item/equip.json")
+
+Tracker:AddLayouts("layouts/shared.json")
+Tracker:AddLayouts("layouts/tracker.json")
+Tracker:AddLayouts("layouts/broadcast.json")
+
+ -- function tracker_on_accessibility_updated()
+ -- end
--- update.bat
+++ update.bat
@@ -1,12 +1,13 @@
 @echo off
 :: This batch script will zip up the contents of the folder its in and drop it in the pack folder
 :: Handy for doing releases
-
+setlocal enabledelayedexpansion
 cd %~dp0
+set currentDir=%~dp0
 
 set packname=dual_mp3cr.zip
+set udo=%OneDrive%\Documents\EmoTracker\packs\%packname%
 set udd=%USERPROFILE%\Documents\EmoTracker\packs\%packname%
-set udo=%USERPROFILE%\OneDrive\Documents\EmoTracker\packs\%packname%
 set udl=%udd% %udo%
 set runfail=0
 set itemlist=0
@@ -20,11 +21,11 @@ echo .
   (if exist %%a (
     (echo %%a located.)
     (del %%a 2>NUL)
-    ("C:\Program Files\7-Zip\7z.exe" a %%a -xr!*.bat -xr!.git* -xr!*.git -xr!*/unused/*)
+    ("C:\Program Files\7-Zip\7z.exe" a %%a %currentDir%/ xr!*.git xr!*.bat xr!*.pdn xr!*_uncompressed* xr!.git*)
     (echo %%a replaced.)
   ) else (
     (set /a runfail=runfail+1)
-    (echo .)
+    (echo %runfail%)
   ))
 ))
 
@@ -33,12 +34,12 @@ echo .
 
 echo .
 if %runfail% GEQ %itemlist% (
-  (del %udd% 2>NUL)
-  ("C:\Program Files\7-Zip\7z.exe" a %udd% -x!*.bat -x!.git* -x!*.git -x!.git/*)
+  (del %udo% 2>NUL)
+  ("C:\Program Files\7-Zip\7z.exe" a %udo% %currentDir%/ xr!*.git xr!*.bat xr!*.pdn xr!*_uncompressed* xr!.git*)
 ) else (
   echo .
 )
 
 ::Test
 
-"%AppData%/Microsoft/Windows/Start Menu/Programs/LightApps/EmoTracker.lnk"
+"C:/ProgramData/Microsoft/Windows/Start Menu/Programs/EmoTracker/EmoTracker.lnk"
